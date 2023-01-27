07f220ac478b7c0922ff412d733781d4c9d8e888
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-01-27 18:24:04 +0530

    Project structure and port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ dotenv.config();
 app.use(cors())
 app.use(express.json());
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server started on port ${process.env.PORT || 5000}`);
 });
