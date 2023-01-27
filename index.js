602b72a1403b4783a0dadf4fe0784265b33c0aa0
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-01-27 19:15:26 +0530

    Creating the user route

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express()
 const dotenv = require('dotenv')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const userRoute = require('./routes/users')
 
 dotenv.config();
 app.use(cors())
 app.use(express.json());
+
+app.use("/api/v1/users", userRoute)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Server started on port ${process.env.PORT || 5000}`);
