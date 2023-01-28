c224a7cc4577d92a839b93cbd2c26ffda6d1080f
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-01-28 16:24:30 +0530

    Creating basic user routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt')
 
+//GET ALL USERS
+router.get('/', (req, res) => {
+    res.send("Get all users")
+})
 
+//GET A SINGLE USER
+router.get('/:id', (req, res) => {
+    res.send(`Get user with id: ${req.params.id}`)
+})
 
+//CREATE A USER
+router.post('/', (req, res) => {
+    res.json(req.body)
+})
+
+//UPDATE A USER 
+router.put('/:id', (req, res) => {
+    res.json(req.body)
+})
 
 module.exports = router;
