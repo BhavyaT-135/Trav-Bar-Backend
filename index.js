c0beeca4c488ef4b45a556f690f0c988f9fcc02f
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-01-28 16:41:55 +0530

    Connected to MongoDB

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const userRoute = require('./routes/users')
+const connectDB = require('./db/connect')
 
 dotenv.config();
 app.use(cors())
 app.use(express.json());
 
+mongoose.set('strictQuery', false)
+
 app.use("/api/v1/users", userRoute)
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server started on port ${process.env.PORT || 5000}`);
-});
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(process.env.PORT || 5000, () => console.log(`Server started on port ${process.env.PORT || 5000}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
